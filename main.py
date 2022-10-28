e1860c0964e4c2f1d4be352688988b71d14604f7
Ada Toydemir <toydemir@usc.edu>
2022-10-27 19:46:59 -0700

    basic sequencer up with a trigger for every measure - can now start filling in video and audio

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from ossaudiodev import SOUND_MIXER_ALTPCM
 import numpy
 import PySimpleGUI as sg
 import os
 import sys
+import time
 
 FRAME_WIDTH = 480
 FRAME_HEIGHT = 270
+BPM = 128;
+BPM_DELTA = 60.0 / BPM; #seconds per beat
 
 frame = numpy.zeros((FRAME_HEIGHT, FRAME_WIDTH, 3), dtype=numpy.uint8)
 
@@ -20,14 +24,23 @@ def main():
     window = sg.Window('videoPlayer.py',
             layout, location=(600, 200))
     play = False
+
     # Main Event Loop for GUI
+
+    time_stamp = time.time()
+
     while True:
         event, values = window.Read(timeout=1)
+        if (time.time() - time_stamp >= BPM_DELTA*4):
+            #SOUND TRIGGER
+
+            #VIDEO TRIGGER
+
+            time_stamp = time.time()
         if event == 'Exit' or event == sg.WIN_CLOSED:
             print('Exiting')
             break
         if event == 'Play':
-            print('Pressed Play')
             play = True
         elif event == 'Pause':
             print('Pressed Pause')
